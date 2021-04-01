a0dd4859835d53e03253a09119f829cc69cc3c81
Y.Jo <jyj0522771@gmail.com>
2021-04-01 23:01:06 +0900

    210401 TIL

---
+++ Study Note/Algorithm Problems/BOJ/12904 A와 B (pass)/BOJ_12904.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        String T = sc.nextLine();
+        
+        int len_s = S.length();
+        int len_t = T.length();
+        
+        while(len_s < len_t)
+        {
+            if (T.charAt(len_t - 1) == 'B')
+                T = (new StringBuffer(T.substring(0, --len_t))).reverse().toString();
+            else
+                T = T.substring(0, --len_t);
+        }
+        if (S.equals(T))
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
