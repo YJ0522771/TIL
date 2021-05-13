f3039f33907957b0b6f64027c135be2d3e62573b
Y.Jo <jyj0522771@gmail.com>
2021-05-13 23:06:21 +0900

    210513 TIL

--- Study Note/DB/SQL_Practices/Programmers/Level2.sql
+++ Study Note/DB/SQL_Practices/Programmers/Level2.sql
@@ -73,3 +73,47 @@ SELECT MIN(DATETIME) AS '시간' FROM ANIMAL_INS
 -- 동물이 몇 마리 들어왔는지 조회
 
 SELECT COUNT(*) AS 'count' FROM ANIMAL_INS
+
+
+-- NULL 처리하기
+
+-- 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회
+-- 이름이 없는 동물의 이름은 "No name"으로 표시
+
+SELECT ANIMAL_TYPE, 
+CASE WHEN NAME IS NULL THEN 'No name'
+ELSE NAME
+END AS 'NAME',
+SEX_UPON_INTAKE FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+
+-- 입양 시각 구하기(1)
+
+-- 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회
+-- 시간대 순으로 정렬
+
+SELECT HOUR(DATETIME), COUNT(*) FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) >= 9 AND HOUR(DATETIME) <= 19
+GROUP BY HOUR(DATETIME)
+ORDER BY HOUR(DATETIME)
+
+
+-- 중복 제거하기
+
+-- 동물의 이름은 몇 개인지 조회
+-- 이름이 NULL인 경우는 집계하지 않으며
+
+-- COUNT(DISTINCT ..)
+
+SELECT COUNT(DISTINCT NAME) AS 'count' FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+
+
+-- DATETIME에서 DATE로 형 변환
+
+-- 동물의 아이디와 이름, 들어온 날짜(시, 분, 초 제외)를 조회
+
+SELECT ANIMAL_ID, NAME, 
+DATE_FORMAT(DATETIME, '%Y-%m-%d') AS '날짜' 
+FROM ANIMAL_INS
